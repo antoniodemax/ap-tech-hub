87a07878a9c45ccc4a11d5698fb8df24bb5b3347
Anthony <anthony.onyango@student.moringaschool.com>
2024-09-29 01:50:04 +0300

    added some css animation in the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const HeroSection = () => {
-    const handleGetStarted = () => {
-        const productsSection = document.getElementById('products');
-        productsSection.scrollIntoView({ behavior: 'smooth' });
-      };
+  const handleGetStarted = () => {
+    const productsSection = document.getElementById('products');
+    productsSection.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
-    <section id="hero">
+    <section id="hero" className="hero-section">
       <div className="hero-content">
-        <h1>Welcome to A.P Tech Hub</h1>
-        <p>Your one-stop shop for tech products</p>
-        <button onClick={handleGetStarted}>Get Started</button>
+        <h1 className="hero-heading">Welcome to A.P Tech Hub</h1>
+        <p className="hero-paragraph">Your one-stop shop for tech products</p>
+        <button className="hero-button" onClick={handleGetStarted}>
+          Get Started
+        </button>
       </div>
     </section>
   );
